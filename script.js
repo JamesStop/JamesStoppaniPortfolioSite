dabd976577b6d81e4deb13fbd93185d8d0da7a53
xelphite <stoppanij@gmail.com>
2022-05-25 19:47:10 -0700

    removed larger nav bar and made so each button is on its own and can all be clicked

--- script.js
+++ script.js
@@ -11,11 +11,14 @@
 
 /*----- cached element references -----*/
 
+const firstButton = document.querySelector('#first');
+const main = document.querySelector('main')
+
 
 
 /*----- event listeners -----*/
 
-
+main.addEventListener('click', something)
 
 
 
@@ -23,3 +26,20 @@
 
 
 /*----- functions -----*/
+
+
+function something(event) {
+    console.log(event)
+    if (event.target.id == 'first') {
+        console.log('1');
+    }
+    if (event.target.id == 'second') {
+		console.log('2');
+	}
+    if (event.target.id == 'third') {
+		console.log('3');
+	}
+    if (event.target.id == 'fourth') {
+		console.log('4');
+	}
+}
