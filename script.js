2f872c57218375810b0060f9ccd4c0ceff9c822f
xelphite <stoppanij@gmail.com>
2022-05-25 18:43:47 -0700

    used sticky position for nav bar, removed smoothscrolling as it conflicts with positioning

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
 /*----- constants -----*/
 
-const ease = 0.1;
-
 
 
 
 
 /*----- app's state (variables) -----*/
 
-let current = 0;
-let target = 0;
-
 
 
 
 /*----- cached element references -----*/
-
-const scroll = document.querySelector('.scroll');
-const main = document.querySelector('main');
 
 
 
@@ -31,64 +23,3 @@ const main = document.querySelector('main');
 
 
 /*----- functions -----*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//function for smoothscrolling
-
-/*
-
-
-function lerp(start, end, t) {
-    return start * (1 - t) + end * t;
-}
-
-function init() {
-    main.style.height = `${scroll.getBoundingClientRect().height}px`
-}
-
-function smoothScroll() {
-    target = window.scrollY;
-    current = lerp(current, target, ease);
-    scroll.style.transform = `translate3d(0, ${-current}px, 0)`;
-    window.requestAnimationFrame(smoothScroll);
-}
-
-init();
-smoothScroll();
-
-*/
